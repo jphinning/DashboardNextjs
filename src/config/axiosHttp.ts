5d2cab986deac5ae6e2c8095fbdd9ae8478045c0
Joao Hinning <jphinning@gmail.com>
2022-04-12 11:34:38 -0300

    Deploy Changes

--- src/config/axiosHttp.ts
+++ src/config/axiosHttp.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://localhost:4000/" ||
-    "https://dashboard-node-jphinning.herokuapp.com/",
+    "https://dashboard-node-jphinning.herokuapp.com/" ||
+    "http://localhost:4000/",
   headers: {
     "Content-type": "application/json",
   },
